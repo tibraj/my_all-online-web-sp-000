829f8ceda495903d66d3bd92624101949077f76e
Taulant Ibraj <tibraj94@gmail.com>
2020-03-30 16:55:50 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,13 +7,11 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+  end
 
 my_all?([1,2,3]) {|i| i < 2}
-
-if block_return_values.include?(false)
-  false
-else
-  true
-end
-end
